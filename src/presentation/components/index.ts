72582b8fe3882d57baae2e6dfce9a5c52ee6f398
Slayther Zamora <slayther.zamora@epn.edu.ec>
2025-04-02 21:18:49 -0500

    feat: add chat bubble components for GptMessage, MyMessage, and GptOrthographyMessage

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,1 +1,5 @@
 export * from './sidebar/SidebarMenuItem';
+
+export * from './chat-bubbles/GptMessage';
+export * from './chat-bubbles/MyMessage';
+export * from './chat-bubbles/GptOrthographyMessage';
